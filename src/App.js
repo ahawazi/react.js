222a983a2bac4ef3b42fc0e820f619c3bf5e85f2
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-21 11:39:43 +0330

    inline function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import "./App.css";
 
 function App() {
   const [age, setAge] = useState(0)
-  const increaseAge = () =>{
-    setAge(age+1)
-  }
   return (
     <div className="App">
       <h1>{age}</h1>
-      <button onClick={increaseAge}>increaseAge</button>
+      <button onClick={()=> setAge(age+1)}>increaseAge</button>
     </div>
   )
 }
