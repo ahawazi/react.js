8376e553b0d94699d39a329575f916e477ab4e51
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-21 11:41:34 +0330

    decrease and reset

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <h1>{age}</h1>
       <button onClick={()=> setAge(age+1)}>increaseAge</button>
+      <button onClick={()=> setAge(age-1)}>decreaseAge</button>
+      <button onClick={()=> setAge(0)}>reset</button>
     </div>
   )
 }
