b7eefaf7e7a26845cb7facf489bc2d1c07464184
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-22 17:35:05 +0330

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import axios from "axios";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { BrowserRouter as router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [catFact, setCatFact] = useState("");
-
-  useEffect(() => {
-    fetchFact();
-  }, []);
-
-  const fetchFact = () => {
-    axios.get("https://catfact.ninja/fact").then((res) => {
-      console.log(res.data);
-      setCatFact(res.data.fact);
-    });
-  };
+  
 
   return (
     <div className="App">
-      <button onClick={fetchFact}>fetch data</button>
-      <p>{catFact}</p>
+      
     </div>
   );
 }
